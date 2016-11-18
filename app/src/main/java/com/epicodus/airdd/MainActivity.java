71347e48846bf1d655bc05f44ce3530eb4ad4df1
Zachary Matthewstearn <zammo911@gmail.com>
2016-11-18 10:38:47 -0800

    initial commit: create main activity skeleton

---
+++ app/src/main/java/com/epicodus/airdd/MainActivity.java
@@ -0,0 +1,43 @@
+package com.epicodus.airdd;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.textView_title) TextView mTitleTextView;
+    @Bind(R.id.button_findGame) Button mFindGameButton;
+    @Bind(R.id.button_hostGame) Button mHostGameButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface modestoBoldFont = Typeface.createFromAsset(getAssets(), "fonts/modesto-bold.ttf");
+        mTitleTextView.setTypeface(modestoBoldFont);
+
+        mFindGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Find Game!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mHostGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Host Game!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}

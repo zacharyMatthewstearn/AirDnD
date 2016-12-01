1e899361898548a248c26c5143be1b3eb241f4dc
Zachary Matthewstearn <zammo911@gmail.com>
2016-12-01 15:57:01 -0800

    add DM/Play toggle buttons to HostGameActivity that toggle eachother appropriately

--- app/src/main/java/com/epicodus/airdd/ui/HostGameActivity.java
+++ app/src/main/java/com/epicodus/airdd/ui/HostGameActivity.java
@@ -2,14 +2,41 @@ package com.epicodus.airdd.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ToggleButton;
 
-import com.epicodus.airdd.ui.R;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class HostGameActivity extends AppCompatActivity {
+public class HostGameActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = HostGameActivity.class.getSimpleName();
+
+    @Bind(R.id.toggleButton_DM) ToggleButton mToggleButtonDM;
+    @Bind(R.id.toggleButton_Play) ToggleButton mToggleButtonPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_game);
+        ButterKnife.bind(this);
+
+        mToggleButtonDM.setOnClickListener(this);
+        mToggleButtonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.toggleButton_DM:
+                mToggleButtonPlay.setChecked(!mToggleButtonPlay.isChecked());
+                break;
+            case R.id.toggleButton_Play:
+                mToggleButtonDM.setChecked(!mToggleButtonDM.isChecked());
+                break;
+            default:
+                Log.d(TAG, "HostGameActivity onClick received bad argument for 'view'");
+        }
     }
 }
